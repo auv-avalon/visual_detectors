e29a1dfab88af9cebeb7a88a24757ff69e54f1c7
Your full name <deshalb@tzi.de>
2011-05-06 13:31:15 +0200

    buoy_pos_estimation header und cpp geaddet

---
+++ src/buoy_pos_estimation.cpp
@@ -0,0 +1,7 @@
+#include "buoy_pos_estimation.h"
+
+namespace avalon{
+    void BuoyPosEstimator::estimateAuvKoordinates(Buoy* buoy)
+    {}
+
+}
---
+++ src/buoy_pos_estimation.h
@@ -0,0 +1,31 @@
+/*
+ * Avalon visual_detectors - buoy_pos_estimation.h
+ * estimates the position of a buoy detected by the camera
+ * 
+ * created by: 
+ *      Thomas Grassow <deshalb@informatik.uni-bremen.de>
+ */
+
+
+#include <cv.h>
+#include <base/samples/frame.h>
+#include <frame_helper/FrameHelperTypes.h>
+#include <frame_helper/FrameHelper.h>
+#include <avalon_base/feature.h>
+#include <base/eigen.h>
+
+namespace avalon{
+	
+    class BuoyPosEstimator{
+        private:
+            
+        public:
+	    
+            //calculates the position of the buoy relativ to the center of the AUV
+            //and stores it into the buoy itself
+            //parameters:
+            //buoy        a pointer to a Buoy
+            static void estimateAuvKoordinates(Buoy* buoy);
+		
+}
+}
