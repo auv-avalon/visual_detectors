9dc55e5446841789222b94ae01b9679c13d3ecb7
Your full name <deshalb@tzi.de>
2011-05-13 13:53:16 +0200

    buoy_pos_estimation can now calculate the position of a buoy relative to the camera

--- src/buoy_pos_estimation.cpp
+++ src/buoy_pos_estimation.cpp
@@ -1,7 +1,28 @@
 #include "buoy_pos_estimation.h"
 
 namespace avalon{
-    void BuoyPosEstimator::estimateAuvKoordinates(Buoy& buoy)
-    {}
 
+    void BuoyPosEstimator::estimateCWKoordinates(feature::Buoy& buoy,frame_helper::FrameHelper fh, base::samples::frame::Frame &frame)
+    {
+        //calculate the distance to buoy (x-koordinate)
+        buoy.world_coord[0]=fh.calcDistanceToObject(frame,buoy.image_radius,buoyRadius,buoy.image_radius,buoyRadius);
+	//calculate the y and z koordinate
+        cv::Point2f point2d=fh.calcRelPosToCenter(frame, buoy.image_x,buoy.image_y, buoy.world_coord[0]);
+        buoy.world_coord[1]=point2d.x;
+        buoy.world_coord[2]=point2d.y;
+    }
+
+
+    base::Vector3d BuoyPosEstimator::estimateAuvKoordinates(feature::Buoy& buoy, base::samples::frame::Frame &frame)
+    {
+        frame_helper::FrameHelper fh = frame_helper::FrameHelper();
+        //estimate world-koordinates relative to camera
+        estimateCWKoordinates(buoy,fh,frame);
+        //transfer the koordinates relative to the AUV
+        //TODO:...
+
+
+        return buoy.world_coord;
+    }
+    
 }
--- src/buoy_pos_estimation.h
+++ src/buoy_pos_estimation.h
@@ -13,19 +13,42 @@
 #include <frame_helper/FrameHelper.h>
 #include <avalon_base/feature.h>
 #include <base/eigen.h>
+#include <vector>
+#include <opencv/cv.h>
+
+
 
 namespace avalon{
 	
     class BuoyPosEstimator{
         private:
-            
+            //der Radius der Boje in mm
+            static const float buoyRadius=300;
+            //die Brennweite der Kamera
+            static const float fx=1000;		//TODO: Echte Werte heraus finden
+            static const float fy=1000;
+
+		//woher kriege ich fx und fy???
+		//woher hat alex die pixel-breite auf dem die
+	        //wo kriege ich breite und höhe des Bildes her?
+
+            static const float imgWidth=1024;
+            static const float imgHeight=640;
+
+
+
+            //helps estimateAuvKoordinates. calculates the position of the buoy
+            //relativ to the camera of the AUV
+            //parameters:
+            //buoy        a pointer to a Buoy
+            void estimateCWKoordinates(avalon::feature::Buoy& buoy,frame_helper::FrameHelper fh, base::samples::frame::Frame &frame);
         public:
 	    
             //calculates the position of the buoy relativ to the center of the AUV
             //and stores it into the buoy itself
             //parameters:
             //buoy        a pointer to a Buoy
-            static void estimateAuvKoordinates(Buoy& buoy);
+            base::Vector3d estimateAuvKoordinates(avalon::feature::Buoy& buoy,base::samples::frame::Frame &frame);
 		
-}
+    };
 }
