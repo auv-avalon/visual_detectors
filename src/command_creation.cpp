9f4d08b8dfb5afa731c7d634f3c591ebb3f7d4a5
mboukamp <Marcel.Boukamp@gmx.de>
2011-07-07 15:42:47 +0200

    added fixed depth

--- src/command_creation.cpp
+++ src/command_creation.cpp
@@ -15,7 +15,7 @@ CommandCreator::CommandCreator(double d) : good_dist(d)
 }
 
 
-base::AUVPositionCommand CommandCreator::centerBuoy(feature::Buoy &buoy, base::samples::RigidBodyState rbs)
+base::AUVPositionCommand CommandCreator::centerBuoy(feature::Buoy &buoy, base::samples::RigidBodyState rbs, double desired_buoy_depth)
 {
         double heading = 0;
         if(buoy.world_coord(0)!=0)
@@ -32,11 +32,11 @@ base::AUVPositionCommand CommandCreator::centerBuoy(feature::Buoy &buoy, base::s
 	if(command.x < -0.2)
 	    command.x = -0.2;
         command.y =0; // no strafing
-        command.z =buoy.world_coord(2)+z;	//depth
+        command.z = desired_buoy_depth;//buoy.world_coord(2)+z;	//depth
     return command;
 }
 
-base::AUVPositionCommand CommandCreator::strafeBuoy(feature::Buoy &buoy, base::samples::RigidBodyState rbs, Direction dir)
+base::AUVPositionCommand CommandCreator::strafeBuoy(feature::Buoy &buoy, base::samples::RigidBodyState rbs, Direction dir, double desired_buoy_depth)
 {
     base::AUVPositionCommand command;
     double z = rbs.position[2];
@@ -46,13 +46,13 @@ base::AUVPositionCommand CommandCreator::strafeBuoy(feature::Buoy &buoy, base::s
         command.x=0;
         command.y=0.5;
         command.heading=(atan(buoy.world_coord(0) / command.y)-M_PI/2)/8;
-        command.z=buoy.world_coord(2)+z;
+        command.z = desired_buoy_depth;//buoy.world_coord(2)+z;	//depth
     break;
     case RIGHT:
         command.x=0;
         command.y=-0.5;
         command.heading=(M_PI/2-atan(buoy.world_coord(0) / command.y))/8;
-        command.z=buoy.world_coord(2)+z;
+        command.z = desired_buoy_depth;//buoy.world_coord(2)+z;	//depth
     break;
     }
     return command;
