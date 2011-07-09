f8a974e2482f8f04609e90b501b050721289850e
Your full name <deshalb@tzi.de>
2011-07-09 10:12:15 +0200

    changes in command_creation

--- src/command_creation.cpp
+++ src/command_creation.cpp
@@ -28,7 +28,9 @@ base::AUVPositionCommand CommandCreator::centerBuoy(feature::Buoy &buoy, base::s
             heading = atan(buoy.world_coord(1) / buoy.world_coord(0));
             heading*=headingFactor;
         }
-        //double z = rbs.position[2];
+        double z = buoy.world_coord(2);
+	if(z>0.3)z=0.25;
+        if(z<-0.3)z=-0.25;
         base::AUVPositionCommand command;
         command.heading =heading;
         command.x = (buoy.world_coord(0) - good_dist)*0.2;  //distance
@@ -38,46 +40,47 @@ base::AUVPositionCommand CommandCreator::centerBuoy(feature::Buoy &buoy, base::s
 	if(command.x < -maxX)
 	    command.x = -maxX;
         command.y = 0; // no strafing
-        command.z = desired_buoy_depth;//buoy.world_coord(2)+z;	//depth
+        command.z = desired_buoy_depth+z;	//depth
     return command;
 }
 
 base::AUVPositionCommand CommandCreator::strafeBuoy(feature::Buoy &buoy, base::samples::RigidBodyState rbs, double intensity, double desired_buoy_depth, double headingFactor, double headingModulation)
 {
     base::AUVPositionCommand command;
-    //double z = rbs.position[2];
+    double z = buoy.world_coord(2);
+    if(z>0.3)z=0.25;
+    if(z<-0.3)z=-0.25;
     double heading = 0;
     if(buoy.world_coord(0)!=0)
     {
         heading = atan(buoy.world_coord(1) / buoy.world_coord(0));
         heading*=headingFactor;
     }
-//TODO:: mark fragen ob ob es ok ist heading in die falsche richtung zunächst auf 0 zu setzen
+
     if(intensity>0){  //strafe nach links
 //        if(heading>0) heading=0;  //hier nicht nach links drehen
         command.x=0;
         command.y=intensity;
         command.heading=heading - headingModulation;
-        command.z = desired_buoy_depth;//buoy.world_coord(2)+z;	//depth
+        command.z = desired_buoy_depth+z;	//depth
     }else{            //strafe nach rechts
 //        if(heading<0) heading=0;  //hier nicht nach rechts drehen
         command.x=0;
         command.y=intensity;
         command.heading=heading + headingModulation;
-        command.z = desired_buoy_depth;//buoy.world_coord(2)+z;	//depth
+        command.z = desired_buoy_depth+z;	//depth
     }
     return command;
 }
 
 base::AUVPositionCommand CommandCreator::cutBuoy(base::samples::RigidBodyState rbs, double desired_buoy_depth, double h)
 {
-    //double z = rbs.position[2]+0.4;
     base::AUVPositionCommand command;
     command.heading =0;
-    command.x = 0.5;  //distance
+    command.x = 0.8;  //distance
 	// cap the maximum x speed
     command.y =0; // no strafing
-    command.z = desired_buoy_depth+h;//buoy.world_coord(2)+z;	//depth
+    command.z = desired_buoy_depth+h;//depth
     return command;
 }
 
@@ -90,13 +93,12 @@ base::AUVPositionCommand CommandCreator::cutBuoy(feature::Buoy &buoy, base::samp
         heading = atan(buoy.world_coord(1) / buoy.world_coord(0));
         heading*=1;
     }
-    //double z = rbs.position[2]+0.4;
     base::AUVPositionCommand command;
     command.heading =heading;
-    command.x = 0.5;  //distance
+    command.x = 0.8;  //distance
 	// cap the maximum x speed
     command.y =0; // no strafing
-    command.z = desired_buoy_depth+h;//buoy.world_coord(2)+z;	//depth
+    command.z = desired_buoy_depth+h;	//depth
     return command;
 }
 
