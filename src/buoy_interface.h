a203bb42142920f5e347856b5b3e18386e1a683a
Fabian <fabian@fabian-Aspire-5820TG.(none)>
2011-10-07 14:02:10 +0200

    Thomas

--- src/buoy_interface.h
+++ src/buoy_interface.h
@@ -43,7 +43,7 @@ class BuoyFilter {
  */
 class BuoyDetector {
  public:
-     virtual BuoyFeatureVector detect(IplImage* image, IplImage* h_plane,IplImage* s_plane) = 0;
+     virtual BuoyFeatureVector detect(IplImage* image, IplImage* h_plane) = 0;
 
      inline static void draw(IplImage* image, const feature::Buoy& buoy,CvScalar color) {
          cvCircle(image, cvPoint(buoy.image_x, buoy.image_y), buoy.image_radius, 
